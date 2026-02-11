5fc435f77149c69040421804aacd753586be72ff
Miroslav Uhlir <mirek.uhlir@gmail.com>
2026-02-11 19:58:10 +0100

    Support multiple CORS origins by parsing the `ALLOWED_ORIGIN` environment variable.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const { PeerServer } = require("peer");
 
 const port = process.env.PORT || 9000;
+const allowedOrigins = process.env.ALLOWED_ORIGIN ? process.env.ALLOWED_ORIGIN.split(',').map(origin => origin.trim()) : [];
+
 PeerServer({ 
     port: port, 
     path: "/",
     proxied: true,
     corsOptions: {
-        origin: process.env.ALLOWED_ORIGIN
+        origin: allowedOrigins
     }
 });
 
